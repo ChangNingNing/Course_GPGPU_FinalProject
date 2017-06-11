33792a392dbe1a7a9aae6268af938e287f1669f0
ChangNingNing <r05922087@ntu.edu.tw>
2017-06-11 17:49:37 +0800

    20170611 implement mySort function with thrust

--- SAP/mySAP.cu
+++ SAP/mySAP.cu
@@ -1,9 +1,19 @@
 #include "mySAP.h"
+#include <thrust/sort.h>
+#include <thrust/execution_policy.h>
 
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 
 /* Cuda Function */
+// mySort's kernel
+struct myCompare{
+	__host__ __device__
+	bool operator()(const Object& a, const Object& b){
+		return a.pos[0] < b.pos[0];
+	}
+};
 
+// myMoveObject's kernel
 __global__ void CudaMoveObject( Object *cuObj, int N, int Boundary, float FT){
 	const float g = -98.0665;	// acceleration of gravity
 	const float coef_rest = 0.9;	// coefficient of restitution
@@ -51,7 +61,11 @@ __global__ void CudaMoveObject( Object *cuObj, int N, int Boundary, float FT){
 #define BlockSize 256
 
 void myFindSweepDirection( Object *cuObj, int SweepDir[3], int N){}
-void mySort( Object *cuObj, int SweepDir[3], int N){}
+
+void mySort( Object *cuObj, int SweepDir[3], int N){
+	thrust::sort(thrust::device, cuObj, cuObj+N, myCompare());
+}
+
 void mySAP( Object *cuObj, int SweepDir[3], int N){}
 
 void myMoveObject( Object *cuObj, int N, int Boundary, float FT){
