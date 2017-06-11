ce2456881b26f40dad4b561f37518d19dff1877b
M-J-LIN <tf000744259223@gmail.com>
2017-06-11 18:19:52 +0800

    mySAP

--- SAP/mySAP.cu
+++ SAP/mySAP.cu
@@ -55,6 +55,40 @@ __global__ void CudaMoveObject( Object *cuObj, int N, int Boundary, float FT){
 		cuObj[id].v[1] += g*FT;
 	}
 }
+//mySAP kernel
+#define sqr(a) (a)*(a)
+__global__ void CudaSAP(Object *cuObj, int SweepDir[3], int N){
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(id >= N) return;
+    float myPos[3], myR;
+    myPos[0] = cuObj[id].pos[0];
+    myPos[1] = cuObj[id].pos[1];
+    myPos[2] = cuObj[id].pos[2];
+    myR = cuObj[id].r;
+
+    int axis;
+    if(SweepDir[0])
+        axis = 0;
+    else if(SweepDir[1])
+        axis = 1;
+    else
+        axis = 2;
+
+    float bound = myPos[axis] + myR;
+    for(int i = id + 1; i < N && (cuObj[i].pos[axis] - cuObj[i].r) <= bound ; i++){
+        float tmpPos[3], tmpR;
+        tmpPos[0] = cuObj[i].pos[0];
+        tmpPos[1] = cuObj[i].pos[1];
+        tmpPos[2] = cuObj[i].pos[2];
+        tmpR = cuObj[i].r;
+        float dist = sqr(tmpPos[0]-myPos[0])+sqr(tmpPos[1]-myPos[1])+sqr(tmpPos[2]-myPos[2]);
+        if(dist <= (sqr(myR) + sqr(tmpR)) )
+            cuObj[i].isCollision = 1;
+
+    }
+
+}
 
 /**/
 
@@ -66,7 +100,10 @@ void mySort( Object *cuObj, int SweepDir[3], int N){
 	thrust::sort(thrust::device, cuObj, cuObj+N, myCompare());
 }
 
-void mySAP( Object *cuObj, int SweepDir[3], int N){}
+void mySAP( Object *cuObj, int SweepDir[3], int N){
+	dim3 grid(CeilDiv(N, BlockSize), 1), block(BlockSize, 1);
+    CudaSAP<<< grid, block >>>(cuObj, SweepDir, N);
+}
 
 void myMoveObject( Object *cuObj, int N, int Boundary, float FT){
 	dim3 grid(CeilDiv(N, BlockSize), 1), block(BlockSize, 1);
